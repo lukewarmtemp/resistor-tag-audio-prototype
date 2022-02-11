f470b9b8d2c8559d1dcd09174488dc31fc8402b0
Luke Yang <74154406+lukewarmtemp@users.noreply.github.com>
2022-02-11 01:38:08 -0500

    Add files via upload

---
+++ Visual-Studio/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.sourceList = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.timer4 = new System.Windows.Forms.Timer(this.components);
+            this.timer5 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(25, 20);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(650, 138);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(438, 34);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(339, 34);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(25, 37);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(127, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(25, 177);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.sourceList);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button7);
+            this.splitContainer1.Panel2.Controls.Add(this.button6);
+            this.splitContainer1.Panel2.Controls.Add(this.button5);
+            this.splitContainer1.Panel2.Controls.Add(this.button4);
+            this.splitContainer1.Panel2.Controls.Add(this.button3);
+            this.splitContainer1.Size = new System.Drawing.Size(658, 234);
+            this.splitContainer1.SplitterDistance = 456;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // sourceList
+            // 
+            this.sourceList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.sourceList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.sourceList.Location = new System.Drawing.Point(0, 0);
+            this.sourceList.MultiSelect = false;
+            this.sourceList.Name = "sourceList";
+            this.sourceList.Size = new System.Drawing.Size(456, 234);
+            this.sourceList.TabIndex = 0;
+            this.sourceList.UseCompatibleStateImageBehavior = false;
+            this.sourceList.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Device";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Channels";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(14, 146);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 5;
+            this.button7.Text = "To Wave";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(14, 192);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(14, 98);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Stop";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(14, 60);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Start";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(14, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Refresh Sources";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 2000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 1000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // timer4
+            // 
+            this.timer4.Interval = 1000;
+            this.timer4.Tick += new System.EventHandler(this.timer4_Tick);
+            // 
+            // timer5
+            // 
+            this.timer5.Interval = 1000;
+            this.timer5.Tick += new System.EventHandler(this.timer5_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(712, 445);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView sourceList;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Timer timer4;
+        private System.Windows.Forms.Timer timer5;
+    }
+}
+
---
+++ Visual-Studio/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private SoundPlayer simpleSound;
+        private SoundPlayer simpleSound2;
+
+        static SerialPort _serialPort;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM4"; //Set your board COM
+            _serialPort.BaudRate = 9600;
+            _serialPort.Open();
+            simpleSound = new SoundPlayer("StartRecord.wav");
+        }
+
+        NAudio.Wave.WaveIn sourceStream = null;
+        NAudio.Wave.DirectSoundOut waveOut = null;
+        NAudio.Wave.WaveFileWriter waveWriter = null;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<NAudio.Wave.WaveInCapabilities> sources = new List<NAudio.Wave.WaveInCapabilities>();
+
+            for (int i = 0; i < NAudio.Wave.WaveIn.DeviceCount; i++)
+            {
+                sources.Add(NAudio.Wave.WaveIn.GetCapabilities(i));
+            }
+
+            sourceList.Items.Clear();
+
+            foreach (var source in sources)
+            {
+                ListViewItem item = new ListViewItem(source.ProductName);
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, source.Channels.ToString()));
+                sourceList.Items.Add(item);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string a;
+            string b;
+            string c;
+            string d;
+            string f;
+            int confirmResult = 0;
+            bool ticks = false;
+            a = _serialPort.ReadExisting();
+            Console.WriteLine(a);
+            if (a != "\r" && ticks == false)
+            {
+                Thread.Sleep(250);
+                b = _serialPort.ReadLine();
+                Thread.Sleep(250);
+                c = _serialPort.ReadLine();
+                Thread.Sleep(250);
+                d = _serialPort.ReadLine();
+                Thread.Sleep(250);
+                f = _serialPort.ReadLine();
+                Thread.Sleep(250);
+
+                if (b == c && b == d && b == f && b != "\r")
+                {
+                    confirmResult = Int32.Parse(b);
+
+                    try
+                    {
+                        string path = "C:\\Users\\GEOG-student\\OneDrive - University of Toronto\\Documents\\100. Luke\\100. School\\130. University of Toronto\\2020-2021\\Courses\\Semester02\\Test.txt";
+                        if (File.ReadAllLines(path).Contains(confirmResult.ToString()))
+                        {
+                            Console.WriteLine("already in");
+                        }
+                        else 
+                        {
+                            button7.PerformClick();
+                            timer1.Interval = 5000;
+
+                            using (StreamWriter sw = File.AppendText(path))
+                            {
+                                
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("this escept");
+                    }
+                }
+            }
+            stopRecording();
+        }
+
+        private void sourceStream_DataAvailable(object sender, NAudio.Wave.WaveInEventArgs e)
+        {
+            if (waveWriter == null) return;
+
+            waveWriter.WriteData(e.Buffer, 0, e.BytesRecorded);
+            waveWriter.Flush();
+        }
+
+        private void recording(string name)
+        {
+            if (sourceList.SelectedItems.Count == 0) return;
+
+            int deviceNumber = sourceList.SelectedItems[0].Index;
+
+            sourceStream = new NAudio.Wave.WaveIn();
+            sourceStream.DeviceNumber = deviceNumber;
+            sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+
+            string part = "C:\\Users\\GEOG-student\\OneDrive - University of Toronto\\Documents\\100. Luke\\100. School\\130. University of Toronto\\2020-2021\\Courses\\Semester02\\";
+            sourceStream.DataAvailable += new EventHandler<NAudio.Wave.WaveInEventArgs>(sourceStream_DataAvailable);
+            waveWriter = new NAudio.Wave.WaveFileWriter(part + name + ".wav", sourceStream.WaveFormat);
+
+            sourceStream.StartRecording();
+        }
+
+        public void stopRecording()
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+            if (sourceStream != null)
+            {
+                sourceStream.StopRecording();
+                sourceStream.Dispose();
+                sourceStream = null;
+            }
+            if (waveWriter != null)
+            {
+                waveWriter.Dispose();
+                waveWriter = null;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (sourceList.SelectedItems.Count == 0) return;
+
+            int deviceNumber = sourceList.SelectedItems[0].Index;
+
+            sourceStream = new NAudio.Wave.WaveIn();
+            sourceStream.DeviceNumber = deviceNumber;
+            sourceStream.WaveFormat = new NAudio.Wave.WaveFormat(44100, NAudio.Wave.WaveIn.GetCapabilities(deviceNumber).Channels);
+
+            sourceStream.DataAvailable += new EventHandler<NAudio.Wave.WaveInEventArgs>(sourceStream_DataAvailable);
+            string part = "C:\\Users\\GEOG-student\\OneDrive - University of Toronto\\Documents\\100. Luke\\100. School\\130. University of Toronto\\2020-2021\\Courses\\Semester02\\";
+            waveWriter = new NAudio.Wave.WaveFileWriter("C:\\Users\\GEOG-student\\OneDrive - University of Toronto\\Documents\\100. Luke\\100. School\\130. University of Toronto\\2020-2021\\Courses\\Semester02\\hello2.wav", sourceStream.WaveFormat);
+
+            sourceStream.StartRecording();
+        }
+
+        int confirmResult = 0;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            string a;
+            string b;
+            string c;
+            string d;
+            string f;
+            bool ticks = false;
+            a = _serialPort.ReadExisting();
+            Console.WriteLine(a);
+            if (a != "\r" && ticks == false)
+            {
+                Thread.Sleep(250);
+                b = _serialPort.ReadLine();
+                Thread.Sleep(250);
+                c = _serialPort.ReadLine();
+                Thread.Sleep(250);
+                d = _serialPort.ReadLine();
+                Thread.Sleep(250);
+                f = _serialPort.ReadLine();
+                Thread.Sleep(250);
+
+                if (b == c && b == d && b == f && b != "\r")
+                {
+                    confirmResult = Int32.Parse(b);
+
+                    try
+                    {
+                        string path = "C:\\Users\\GEOG-student\\OneDrive - University of Toronto\\Documents\\100. Luke\\100. School\\130. University of Toronto\\2020-2021\\Courses\\Semester02\\Test.txt";
+                        if (File.ReadAllLines(path).Contains(confirmResult.ToString()))
+                        {
+                            Console.WriteLine("already in");
+                            
+                            timer5.Start();
+                            timer2.Stop();
+                        }
+                        else
+                        {
+                            timer4.Start();
+                            timer2.Stop();
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("this escept");
+                    }
+                }
+            }
+        }
+
+        bool first = false;
+        int count = 0;
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            if (first == false)
+            {
+                Console.WriteLine("Recording Start");
+                recording(confirmResult.ToString());
+                first = true;
+            }
+            if (count < 5)
+            {
+                count++;
+            }
+            else if (count >= 5)
+            {
+                string path = "C:\\Users\\GEOG-student\\OneDrive - University of Toronto\\Documents\\100. Luke\\100. School\\130. University of Toronto\\2020-2021\\Courses\\Semester02\\Test.txt";
+                count = 0;
+                stopRecording();
+                Console.WriteLine("Recording End");
+                using (StreamWriter sw = File.AppendText(path))
+                {
+                    sw.WriteLine(confirmResult.ToString());
+                }
+                first = false;
+                timer3.Stop();
+                timer2.Start();
+            }
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+        bool first2 = false;
+        int count2 = 0;
+        private void timer4_Tick(object sender, EventArgs e)
+        {
+            if (first2 == false)
+            {
+                simpleSound.Play();
+                first2 = true;
+            }
+            if(count2 < 3)
+            {
+                count2++;
+            }
+            else if (count2 >= 3)
+            {
+                count2 = 0;
+           
+                first2 = false;
+                timer4.Stop();
+                timer3.Start();
+            }
+        }
+
+        bool first3 = false;
+        int count3 = 0;
+        private void timer5_Tick(object sender, EventArgs e)
+        {
+            if (first3 == false)
+            {
+                simpleSound2 = new SoundPlayer("C:\\Users\\GEOG-student\\OneDrive - University of Toronto\\Documents\\100. Luke\\100. School\\130. University of Toronto\\2020-2021\\Courses\\Semester02\\" + confirmResult.ToString() + ".wav");
+                simpleSound2.Play();
+                first3 = true;
+            }
+            if (count3 < 5)
+            {
+                count3++;
+            }
+            else if (count3 >= 5)
+            {
+                count3 = 0;
+
+                first3 = false;
+                timer5.Stop();
+                timer2.Start();
+            }
+        }
+    }
+}
